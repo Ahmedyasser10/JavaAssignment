9bd402176a4c72cdc053af0958e1b289949afecf
Ahemd Yasser <102255667+Ahmedyasser10@users.noreply.github.com>
2023-02-26 16:39:52 +0000

    Add files via upload
    XOboard

---
+++ XOBoard.java
@@ -0,0 +1,96 @@
+public class XOBoard extends Board{
+    public XOBoard(int n){
+        super(n);
+    }
+    @Override
+    public boolean updateBoard(int x, int y, char symbol) {
+        if (x < 0 || x >= n || y < 0 || y >= n) {
+            return false;
+        }
+        if(grid[x][y] == 'o' || grid[x][y] == 'x') {
+            return false;
+        }
+        grid[x][y] = symbol;
+        return true;
+    }
+    @Override
+    public void displayBoard(){
+        for(char[] i: grid){
+            for(char e: i) {
+                if(e != 'x' && e != 'o')
+                    System.out.print(" | " + "_" + " | ");
+                else {
+                    System.out.print(" | " + e + " | ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    @Override
+    public boolean isWinner(char x) {
+        //columns
+        boolean flag = true;
+        for (int i = 0; i < grid.length; i++) {
+            flag = true;
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] != x) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+
+        //rows
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                flag = true;
+                if (grid[j][i] != x) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                return true;
+            }
+        }
+
+        //positive diagonal
+        for (int i = 0, j = 0; i < n; i++, j++) {
+            flag = true;
+            if(grid[i][j] != x){
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag) {
+            return true;
+        }
+
+        // negative diagonal
+        for (int i = n-1, j = n-1; i >= 0; i--, j--) {
+            flag = true;
+            if(grid[i][j] != x){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    @Override
+    public boolean isDraw(){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] != 'x' && grid[i][j] != 'o')
+                    return false;
+            }
+        }
+        return true;
+    }
+
+}
